3a7552bda539aa4ca9f82bd4e740359cbbb581a1
Mohit Sarkar <mohitsarkar256@gmail.com>
2023-03-23 20:27:08 +0530

    Hello world

--- wel1.py
+++ wel1.py
@@ -1,9 +1,2 @@
 print("Hello world")
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
 print("This is original chandan's repo and previous repo is fake thank you")
->>>>>>> chandan
-=======
-print("Thank you")
->>>>>>> login
