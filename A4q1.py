d3f74205b164f089f9cc4afa025fea29cafefe49
Mohit Sarkar <mohitsarkar256@gmail.com>
2023-03-24 10:06:20 +0530

    assignment 4 uploaded

---
+++ A4q1.py
@@ -0,0 +1,17 @@
+x= 90
+print(x)
+
+name= "Mohit sarkar"
+print(name)
+
+a,b,c= 12,32,1
+print(a)
+print(b)
+print(c)
+
+a1,a2,a3,a4,a5= 12, 12.80, True, 2+4j, "Mohit"
+print(a1,type(a1)) 
+print(a2,type(a2)) 
+print(a3,type(a3)) 
+print(a4,type(a4)) 
+print(a5,type(a5)) 
