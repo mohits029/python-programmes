26eb1cd49ae3f1356518360f54f49eb33f9a02ea
Mohit Sarkar <mohitsarkar256@gmail.com>
2023-03-24 11:17:23 +0530

    Assignment-7 uploaded

---
+++ A7q1.py
@@ -0,0 +1,5 @@
+name= str(input("Please enter your name : "))
+print(name)
+
+a,b= eval(input("Enter two number seprated by comma: "))
+print(a+b)
---
+++ A7q2.py
@@ -0,0 +1,4 @@
+import math
+r= int(input("Enter the radius of the circle: "))
+print(math.pi *r*r)
+
