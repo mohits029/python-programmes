8e6d5bd8647105ce658bba04f9ff172d816e07a5
Mohit Sarkar <mohitsarkar256@gmail.com>
2023-03-24 12:08:11 +0530

    assignment 10 uploaded

---
+++ A10q1.py
@@ -0,0 +1,8 @@
+i= int(input("Enter a number: "))
+print(i//10)
+print(i%10)
+
+a,b= 12,45
+a,b= b,a
+print(a)
+print(b)
---
+++ A10q3.py
@@ -0,0 +1,4 @@
+i= int(input('Enter three digit number: '))
+print(i//100)
+
+print(i//10%10)
